43c97da8b46b1c21bcd8f6237549fff6611a2773
pooja shree P K <46344725+senoritapooja@users.noreply.github.com>
2019-08-02 10:21:09 +0530

    Create sort_array.c

---
+++ sort_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n,a[100],temp;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++){
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;}
+        }    
+    }
+    for(int i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }return 0;
+}
+
+    
